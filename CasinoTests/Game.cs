b12e1be431f17763fac8d147bb81fd156c6c95c4
Yana Khodartsevich <y.khodartsevich@dodopizza.com>
2018-08-02 15:32:13 +0300

    extract Game class

---
+++ CasinoTests/Game.cs
@@ -0,0 +1,15 @@
+﻿namespace TestProject1
+{
+    public class Game
+    {
+        public int playersCount;
+
+        public void WithNumberOfPlayers(int players)
+        {
+            for (var i = 0; i < players; i++)
+            {
+                playersCount += 1;
+            }
+        }
+    }
+}
--- CasinoTests/GameTest.cs
+++ CasinoTests/GameTest.cs
@@ -13,17 +13,4 @@ namespace TestProject1
             Assert.That(game.playersCount <= 6);
         }
     }
-
-    public class Game
-    {
-        public int playersCount;
-
-        public void WithNumberOfPlayers(int players)
-        {
-            for (var i = 0; i < players; i++)
-            {
-                playersCount += 1;
-            }
-        }
-    }
 }
